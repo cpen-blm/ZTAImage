d415d7998b31d2dd72a2cdc47d101ca84c712902
cpen <cpendergast@blm.gov>
2023-08-30 23:11:57 -0600

    Delete NewZT2019ImageTemplateSpec.ps1

--- NewZT2019ImageTemplateSpec.ps1
+++
@@ -1,9 +0,0 @@
-New-AzTemplateSpec `
-    -Name ZT-2019-base-image-ts `
-    -ResourceGroupName blm-computeGallery-uw2-rg `
-    -Version '1.0' `
-    -Location 'westus2' `
-    -DisplayName "Zero Trust 2019 Base Image Template" `
-    -TemplateFile 'C:\Users\cpendergast\OneDrive - DOI\scripts\imaging\solution2019.json' `
-    -UIFormDefinitionFile 'C:\Users\cpendergast\OneDrive - DOI\scripts\imaging\uiDefinition2019.json' `
-    -Force
